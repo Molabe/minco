e038165dfe6f4ad53fb9d86bfd8d410e3a2421cd
Timotej Sedláček <timotejs48@gmail.com>
2024-12-06 16:28:08 +0100

    .

--- server/app.js
+++ server/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+require('dotenv').config();
+
 // Serve static files
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 // Start server
